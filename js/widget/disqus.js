2f566361b6cd76011a3811b6407c7a71a4e27492
Krzysztof Kula <krzychukula@gmail.com>
2013-09-11 17:02:19 +0200

    bridge

--- js/widget/disqus.js
+++ js/widget/disqus.js
@@ -16,7 +16,7 @@ define('js/widgets/disqus', ['alf'], function(Alf){
             function openComments(){
                 console.log('Comments for: ' + shortname + ' ' + query);
                 var url = 'http://disqus.com/embed/comments?f='+shortname+'&t_i='+query+'&s_o=default';
-                app.event.trigger('closedBrowser', url);
+                app.bridge.trigger('closedBrowser', url);
             }
 
             $this.click(openComments);
